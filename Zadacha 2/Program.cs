fb95283c11399107d549ca481d1b7a3c52780d9f
Natalyaprism <130831642+Natalyaprism@users.noreply.github.com>
2023-04-15 12:36:17 +0300

    Add files via upload

---
+++ Zadacha 2/Program.cs
@@ -0,0 +1,22 @@
+﻿int a, b;
+Console.WriteLine("Введите два целых числа");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+ Console.WriteLine("Большее число = "+a);   
+}
+else
+{
+    Console.WriteLine("Большее число = "+b);
+}
+
+
+//Задача 2: Напишите программу, которая на вход принимает 2 числа и
+// какое число большее, а какое меньшее.
+//   a = 5; b = 7 -> max = 7
+//   a = 2 b = 10 -> max = 10
+//   a = -9 b = -3 -> max = -3
+
+
+
